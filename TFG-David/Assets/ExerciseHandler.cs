aba0fd5d3ece27bcaba0daf606da520f3990de47
David Carneros Prado <David.Carneros1@alu.uclm.es>
2020-03-08 13:55:35 +0100

    Añadidos script para leer los ejercicios guardados

---
+++ TFG-David/Assets/ExerciseHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExerciseHandler : MonoBehaviour {
+
+    public Exercise exercise;
+    GameObject view;
+    // Start is called before the first frame update
+    void Start () {
+        view = transform.parent.gameObject;
+
+        foreach (Transform eachChild in transform) {
+            
+            if (eachChild.name == "NameWhatYouNeed") {
+                Debug.Log ("Child found. Mame: " + eachChild.name);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void caseChild(Transform child){
+
+        switch (child.name)
+        {
+            case "RigthHand" : 
+             if(this.exercise.hand == "Right"){
+                    child.gameObject.SetActive(true);
+                }else{
+                    child.gameObject.SetActive(false);
+
+                }
+                break;
+            case "LeftHand" : 
+                if(this.exercise.hand == "Left"){
+                    child.gameObject.SetActive(true);
+                }else{
+                    child.gameObject.SetActive(false);
+
+                } 
+                break;
+            
+            case "DoButton" : 
+
+                break;
+            
+            case "Temporalidad" : 
+                    
+                break;
+            
+            case "NombreEj" : 
+
+                break;
+
+            case "Exactitud" : 
+
+                break;
+
+            default:
+            break;
+        }
+
+
+    }
+}
---
+++ TFG-David/Assets/viewExercises.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text.Json;
+
+
+public class viewExercises : MonoBehaviour
+{
+
+    string exercisesDirectory = @"\Ejercicios";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadExercises(){
+        try {
+            var exercises = Directory.EnumerateFiles(exercisesDirectory, "*.json");
+            var jsonString;
+            Exercise exer;
+            foreach (string currentExercise in exercises)
+            {
+                jsonString = File.ReadAllText(currentExercise);
+                exer = JsonSerializer.Deserialize<Exercise>(jsonString);
+            }
+        }
+        catch(Exception e)
+        {
+             
+        }
+        
+    }
+}
